16e4cb1528aa58dcda3b72bb6692762fa1581c53
soniabilal <soniabilal131358@gmail.com>
2025-08-01 12:29:14 +0500

    added a operators methods

---
+++ operators/operator.py
@@ -0,0 +1,59 @@
+# aitrhmetics opertaor
+#  += Add and assigD
+#  -= Subtract and assigD
+#  *= Multiply and assignA
+#  /= Divide and assigD
+#  //= Floor divide and assigD
+#  %= modulus and assigD
+#  **= Exponentiation and assign
+
+a=32
+b=15
+print(a+b)
+print(b-a)
+print(a*b)
+print(a/b)
+print(a//b)
+print(a**b)
+
+
+# 5 Comparison operators, also called relational operators, are
+# used to compare two values
+# 5 Comparison operators will always provide Boolean result that
+# is True and False
+# 5 comparison operators are as follow
+# 5 == Equal t<
+# 5 != Not Equal to3
+# 5 > Greater than3
+# 5 < Less than 3
+# 5 >= Greater than or equal t<
+# 5 <= Less than or equal t<
+# 5 Comparison operators will work with numbers but you can
+# use them with strings as well.3
+# 5 Strings will be comparing the Ascii values of string.
+# 5 Logical operators in Python are used to combine multiple
+# conditions and return a Boolean result (True or False)
+# 5 There are 3 types of logical operator3
+# 5 and - Return True if both condition are Tru\
+# 5 or - Return True if at least one condition is True
+# a=20
+# a+=40
+# a+=60
+# a+=80
+
+# print(a)
+
+c=13
+d=56
+print(c==d)
+print(c!=d)
+
+print(c>d)
+print(d<c)
+
+
+
+
+
+
+
