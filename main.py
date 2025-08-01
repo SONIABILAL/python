091c911c92d922061b8b17dc37cac11cf0fd6546
soniabilal <soniabilal131358@gmail.com>
2025-08-01 11:34:43 +0500

    added a differnt functionaliy

--- main.py
+++ main.py
@@ -24,3 +24,52 @@ print(type(b))
 string="heloo"
 # data type booleans
 bolean=True
+# string indexing
+a="hello"
+print(a[3])
+# string slicing 
+#  eg - a = “hello” a[1:4:1] ==> output “ell:
+b="hello world"
+# print(a[1:3:1])
+# ye end point tk jaye ga
+# print (b[5::]) 
+# ye 0 sae lekr end tk print kry ga
+print (b[::])
+
+""""type conversion yani ek type ko dosri type ma change krna"""
+# int to string 
+# a=12
+# a=str(a)
+# print(type(a))
+# string to int
+# a = "coder"
+# a = int(a)  # ❌ Error: "coder" is not a number
+# print(type(a))
+
+a = "123"
+a = int(a)  # ✅ Works fine
+print(type(a))  # <class 'int'>
+
+a = "decoder"
+try:
+    a = int(a)
+    print(type(a))
+except ValueError:
+    print("Conversion failed: Not a number")
+
+    """"boolean values conversions"""
+    c=0
+    print(bool(c))
+
+# input and output
+names="python coder"
+age="23"
+# print("my name is ",names,"and my age is",age)
+# es code ko hm format string ma use kry gy
+print(f"my name is {names} and my age is {age}")
+
+# input and output 
+
+input("Enter your name: ")
+print("Hello,", name)
+
