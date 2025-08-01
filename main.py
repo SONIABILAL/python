f3133919312f5f568c0386b3ef236a4623f00423
soniabilal <soniabilal131358@gmail.com>
2025-08-01 09:52:14 +0500

    Initial commit

---
+++ main.py
@@ -0,0 +1,26 @@
+print("hello world")
+
+#hi i am sonia bilal i am from gujranwala
+""""    this is multiple comment
+krne ka tariqa but this is not a comment this is a doc string
+ jis ma multiple para ko hm store kr skty ha
+
+
+ """
+
+#  declare variable 
+name="sonia"
+name1="bilal"
+# dont use this due to declare variable
+# 1name=""
+# nam es=""
+# $hi=""
+a=34
+b=23.4
+c=12 / 2
+print(type(b))
+# data type string
+
+string="heloo"
+# data type booleans
+bolean=True
