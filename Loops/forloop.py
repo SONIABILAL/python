5ed356b70206b68b00bdd32ba2c41ea3057aa687
soniabilal <soniabilal131358@gmail.com>
2025-08-03 15:36:53 +0800

    writing questions with answers in for loop

--- Loops/forloop.py
+++ Loops/forloop.py
@@ -46,11 +46,53 @@ for i in range(len(name)):
 # for loops questions with answers
 # Accept an integer and Print hello world n times
 # Accept an integer from the user
-n = int(input("Enter a number: "))
+# n = int(input("Enter a number: "))
 
 # Print "Hello World" n times
-for i in range(n):
-    print("Hello World")
+# for i in range(n):
+#     print("Hello World")
+# - Print natural number up to n 
+# n = int(input("Enter a number: "))
+
+# for i in range(1, n + 1):
+#     print(i)
+
+#     n = int(input("Enter a number: "))
+    # Reverse for loop. Print n to 1 
+n = int(input("Enter a number: "))
+for i in range(n, 0, -1):
+    print(i)
 
 
+#  Take a number as input and print its table 
+num = int(input("Enter a number: "))
+
+for i in range(1, 11):
+    print(f"{num} x {i} = {num * i}")
+# - Sum up to n terms 
        
+n = int(input("Enter a number: "))
+total = 0
+
+for i in range(1, n + 1):
+    total += i
+
+print("Sum up to", n, "terms is:", total)
+# factorial numbers
+num = int(input("Enter a number: "))
+factorial = 1
+
+for i in range(1, num + 1):
+    factorial *= i
+
+print("Factorial of", num, "is:", factorial)
+# even and odd numbers ka sum
+n=int(input("tell your number"))
+even=0
+odd=0
+for i in range(1,n+1):
+     if i%2==0:
+          even=even+i
+     else:
+          odd=odd+i
+          print(f"even is {even} and odd is {odd}")
